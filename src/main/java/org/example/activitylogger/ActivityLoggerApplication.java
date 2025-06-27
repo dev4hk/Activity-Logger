eab167e631bc54b545b9f42e57e886d1dc3f5bd7
Hoon <hoon0661@gmail.com>
2025-06-27 10:38:53 -0400

    initial commit - project created

---
+++ src/main/java/org/example/activitylogger/ActivityLoggerApplication.java
@@ -0,0 +1,13 @@
+package org.example.activitylogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityLoggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/activitylogger/ActivityLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.activitylogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
